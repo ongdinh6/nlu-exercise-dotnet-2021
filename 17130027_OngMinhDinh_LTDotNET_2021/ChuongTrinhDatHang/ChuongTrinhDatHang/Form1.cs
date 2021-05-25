fdb6445f4d52ef8dcd35ebe7b1a93a5628b5ca90
ongdinh6 <ongdinh6@gmail.com>
2021-05-25 15:20:16 +0700

    add chuongtrinhdathang

---
+++ 17130027_OngMinhDinh_LTDotNET_2021/ChuongTrinhDatHang/ChuongTrinhDatHang/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuongTrinhDatHang
+{
+    public partial class Form1 : Form
+    {
+        ISet<string> listOrderItems = new HashSet<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (string i in listBoxMenu.SelectedItems)
+            {
+                listOrderItems.Add(i.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadMenuItems();
+        }
+
+        void LoadMenuItems()
+        {
+            listBoxMenu.SelectionMode = SelectionMode.MultiSimple;
+            List<string> menus = new List<string>();
+            menus.Add("Doraemon");
+            menus.Add("Kính Vạn Hoa");
+            menus.Add("Harry Potter");
+            //load data form db
+            foreach (string item in menus)
+            {
+                listBoxMenu.Items.Add(item);
+            }
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            LoadDataConsumer();
+        }
+
+        void LoadDataConsumer()
+        {
+            string name = txtName.Text;
+            string phone = txtPhone.Text;
+            string message = "Vui lòng điền đầy đủ thông tin";
+            string caption = "Warning";
+            DialogResult result;
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            if (name == null || name.Equals("") || name.Length == 0 || phone.Length == 0)
+            {
+                result = MessageBox.Show(message, caption, buttons);
+            }
+            else
+            {
+                if (listOrderItems.Count == 0)
+                {
+                    message = "Vui lòng chọn mặt hàng mà bạn muốn đặt.";
+                    result = MessageBox.Show(message, caption, buttons);
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("Thông tin đặt hàng\n");
+                    sb.Append("Khách hàng: " + name + "\n");
+                    sb.Append("Điện thoại: " + phone + "\n");
+                    sb.Append("Sản phẩm đặt mua: " + "\n");
+                    foreach (string myOrder in listOrderItems)
+                    {
+                        sb.Append("- " + myOrder + "\n");
+                    }
+                    listOrderItems.Clear();
+                    lblShow.Text = sb.ToString();
+                    txtName.Clear();
+                    txtPhone.Clear();
+                    listBoxMenu.SelectedItems.Clear();
+                }
+            }
+        }
+
+        private void txtPhone_TextChanged(object sender, EventArgs e)
+        {
+            string caption = "Warning";
+            DialogResult result;
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            string phone = txtPhone.Text;
+            try
+            {
+                foreach(char c in phone.ToCharArray())
+                if (!phone.Equals("") && Char.IsLetter(c)) throw new FormatException("Số điện thoại không hợp lệ");
+            }catch(FormatException ex) 
+            {
+                string message = ex.Message;
+                result = MessageBox.Show(message, caption, buttons);
+                txtPhone.Clear();
+            }
+        }
+    }
+}
---
+++ ChuongTrinhDatHang/ChuongTrinhDatHang/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace ChuongTrinhDatHang
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxMenu = new System.Windows.Forms.ListBox();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From Đặt Hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Khách hàng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Điện thoại";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(89, 68);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(198, 23);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(89, 102);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(198, 23);
+            this.txtPhone.TabIndex = 4;
+            this.txtPhone.TextChanged += new System.EventHandler(this.txtPhone_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Danh sách sản phẩm";
+            // 
+            // listBoxMenu
+            // 
+            this.listBoxMenu.FormattingEnabled = true;
+            this.listBoxMenu.ItemHeight = 15;
+            this.listBoxMenu.Location = new System.Drawing.Point(343, 105);
+            this.listBoxMenu.Name = "listBoxMenu";
+            this.listBoxMenu.Size = new System.Drawing.Size(229, 244);
+            this.listBoxMenu.TabIndex = 6;
+            this.listBoxMenu.SelectedIndexChanged += new System.EventHandler(this.listBoxMenu_SelectedIndexChanged);
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(13, 141);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(128, 41);
+            this.btnOrder.TabIndex = 7;
+            this.btnOrder.Text = "Đặt Hàng";
+            this.btnOrder.UseCompatibleTextRendering = true;
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.BackColor = System.Drawing.Color.White;
+            this.lblShow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblShow.Location = new System.Drawing.Point(13, 212);
+            this.lblShow.MinimumSize = new System.Drawing.Size(270, 135);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(270, 135);
+            this.lblShow.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.listBoxMenu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Chương trình đặt hàng";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBoxMenu;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.TextBox txtPhone;
+    }
+}
+
